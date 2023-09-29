93841de6b070965fefcb531038bf5cf8e16e8e1a
smart-life-tech <christlightworld@gmail.com>
2023-09-29 17:18:12 +0100

    modified:   stepperLimits.ino

--- stepperLimits.ino
+++ stepperLimits.ino
@@ -3,39 +3,39 @@
 const int stepsPerRevolution = 2048;
 Stepper myStepper(stepsPerRevolution, 9, 11, 10, 12);
 
-const int limitSwitch1Pin = 2;  // Pin connected to the first limit switch
-const int limitSwitch2Pin = 3;  // Pin connected to the second limit switch
+const int limitSwitch1Pin = 2; // Pin connected to the first limit switch
+const int limitSwitch2Pin = 3; // Pin connected to the second limit switch
 
-void setup() {
-  myStepper.setSpeed(5);
-  Serial.begin(9600);
+void setup()
+{
+    myStepper.setSpeed(5);
+    Serial.begin(9600);
 
-  pinMode(limitSwitch1Pin, INPUT);
-  pinMode(limitSwitch2Pin, INPUT);
+    pinMode(limitSwitch1Pin, INPUT);
+    pinMode(limitSwitch2Pin, INPUT);
 }
 
-void loop() {
-  // Move the motor clockwise until the first limit switch is hit
-  Serial.println("Moving clockwise");
-  while (digitalRead(limitSwitch1Pin) != HIGH) {
-    myStepper.step(1);
-    delay(10);
-  }
+void loop()
+{
+    // Move the motor clockwise until the first limit switch is hit
+    Serial.println("Moving clockwise");
+    while (digitalRead(limitSwitch1Pin) != HIGH)
+    {
+        // myStepper.step(1);
+        myStepper.setSpeed(12); // rpm
+        myStepper.step(-2048);  // do 2048 steps -- corresponds to one revolution in one minute
+        delay(3600000);
+    }
 
-  // Reverse the motor direction
-  Serial.println("Reversing direction");
-  //myStepper.step(-stepsPerRevolution);
-  delay(500);
+    delay(500);
 
-  // Move the motor counterclockwise until the second limit switch is hit
-  Serial.println("Moving counterclockwise");
-  while (digitalRead(limitSwitch2Pin) != HIGH) {
-    myStepper.step(-1);
-    delay(10);
-  }
-
-  // Reverse the motor direction
-  Serial.println("Reversing direction");
-  //myStepper.step(stepsPerRevolution);
-  delay(500);
+    // Move the motor counterclockwise until the second limit switch is hit
+   // Serial.println("Moving counterclockwise");
+    while (digitalRead(limitSwitch2Pin) != HIGH)
+    {
+        myStepper.setSpeed(12); // rpm
+        myStepper.step(-2048);  // do 2048 steps -- corresponds to one revolution in one minute
+        delay(3600000);
+    }
+    delay(500);
 }
